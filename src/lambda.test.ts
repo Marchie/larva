2c059c0b3010ed58cc1f28e6f3a9ceca70097811
Chris March <Chris.March@tfgm.com>
2022-02-10 19:04:10 +0000

    Let's break a test

--- src/lambda.test.ts
+++ src/lambda.test.ts
@@ -46,7 +46,7 @@ Then the status code is 500 and the body describes the error"`, async () => {
 
     const expectation: APIGatewayProxyResultV2 = {
       body: `ACCOUNT_ID is not defined`,
-      statusCode: 500,
+      statusCode: 502,
     };
 
     expect(result).toEqual(expectation);
